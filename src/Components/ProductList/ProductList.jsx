376acfaa80e0639bdc1317001e64ea8e8f2019b3
radhi aissa <96472173+radhi75@users.noreply.github.com>
2022-02-03 21:33:22 +0100

    Add files via upload

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -1,24 +1,61 @@
 import "./ProductList.css";
 import { products } from "../../data";
-
+import { Rating } from "@mui/material";
+const handleProduct = (x, y) => {
+  alert(x + "it is price is" + y);
+};
 const ProductList = () => {
   return (
     <section>
-      <div className="pl-list">
+      <h2 className="title">This is a product List</h2>
+      <div className="products-list">
         {products.map((item) => (
-          <div className="p">
-            <div className="p-browser">
-              <div className="p-circle"></div>
-              <div className="p-circle"></div>
-              <div className="p-circle"></div>
-            </div>
-            <div>
-              <h2>{item.title} </h2>
-              <img src={item.image} alt="" className="p-img" />
-              <p className="p-desc">{item.description}</p>
-            </div>
+          <div className="product-card">
+            <img src={item.image} alt="" className="p-img" />
+            <h5 className="pl-title">{item.title}</h5>
+            <button onClick={() => handleProduct(item.title, item.price)}>
+              CLICK TO KNOW PRICE
+            </button>
+            <Rating
+              name="half-rating"
+              defaultValue={item.rating.rate}
+              precision={0.5}
+              readOnly
+            />
+            <p className="pl-desc">{item.description}</p>
           </div>
         ))}
+      </div>
+      <h2>
+        list of product that their category is men's clothing and their price
+        greater than 100
+      </h2>
+      <div className="products-list">
+        {products
+          .filter(
+            (item) => item.category === "men's clothing" && item.price > 100
+          )
+          .map((item) => (
+            <div className="product-card">
+              <img src={item.image} alt="" className="p-img" />
+              <h5 className="pl-title">{item.title}</h5>
+              <button
+                onClick={() =>
+                  alert(item.title + "it is price is" + item.price)
+                }
+              >
+                CLICK TO KNOW PRICE
+              </button>
+              <Rating
+                name="half-rating"
+                d
+                defaultValue={item.rating.rate}
+                precision={0.5}
+                readOnly
+              />
+              <p className="pl-desc">{item.description}</p>
+            </div>
+          ))}
       </div>
     </section>
   );
